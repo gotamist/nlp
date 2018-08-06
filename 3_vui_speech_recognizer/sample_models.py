b6e69e7c1f52321625d71ac46db6082ae3144581
The Gotamist <gotamist@gmail.com>
2018-08-06 16:02:03 -0700

    Complete bidirectional_rnn_model()

--- 3_vui_speech_recognizer/sample_models.py
+++ 3_vui_speech_recognizer/sample_models.py
@@ -126,9 +126,9 @@ def bidirectional_rnn_model(input_dim, units, output_dim=29):
     # Main acoustic input
     input_data = Input(name='the_input', shape=(None, input_dim))
     # TODO: Add bidirectional recurrent layer
-    bidir_rnn = ...
+    bidir_rnn = Bidirectional( GRU( units, activation='relu', return_sequences=True, implementation=2 ) )(input_data)
     # TODO: Add a TimeDistributed(Dense(output_dim)) layer
-    time_dense = ...
+    time_dense = TimeDistributed(Dense(output_dim))(bidir_rnn)
     # Add softmax activation layer
     y_pred = Activation('softmax', name='softmax')(time_dense)
     # Specify the model
