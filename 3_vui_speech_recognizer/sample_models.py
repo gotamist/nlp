7dfd03b08ca929b051fafd1ceb43815e3569de85
The Gotamist <gotamist@gmail.com>
2018-08-08 21:43:07 -0700

    Implement a final_model() and cnn_pooling_output_length()

--- 3_vui_speech_recognizer/sample_models.py
+++ 3_vui_speech_recognizer/sample_models.py
@@ -1,7 +1,7 @@
 from keras import backend as K
 from keras.models import Model
 from keras.layers import (BatchNormalization, Conv1D, Dense, Input, 
-    TimeDistributed, Activation, Bidirectional, SimpleRNN, GRU, RNN)
+    TimeDistributed, Activation, Bidirectional, SimpleRNN, GRU, Dropout, MaxPooling1D)
 
 def simple_rnn_model(input_dim, output_dim=29):
     """ Build a recurrent network for speech 
@@ -137,18 +137,121 @@ def bidirectional_rnn_model(input_dim, units, output_dim=29):
     print(model.summary())
     return model
 
-def final_model():
+def deep_bd_rnn_model(input_dim, units, recur_layers=2, output_dim=29):
+    """ Build a deepbidirectional recurrent network for speech
+    """
+    # Main acoustic input
+    input_data = Input(name='the_input', shape=(None, input_dim))
+    # TODO: Add bidirectional recurrent layer
+    recur = input_data
+    for cellnum in range(recur_layers):
+        recur = Bidirectional( GRU( units, activation='relu', return_sequences=True) )(recur)
+        recur = BatchNormalization()(recur)
+    
+    # TODO: Add a TimeDistributed(Dense(output_dim)) layer
+    time_dense = TimeDistributed(Dense(output_dim))( recur )
+    # Add softmax activation layer
+    y_pred = Activation('softmax', name='softmax')(time_dense)
+    # Specify the model
+    model = Model(inputs=input_data, outputs=y_pred)
+    model.output_length = lambda x: x
+    print(model.summary())
+    return model
+
+#def final_model():
+#    """ Build a deep network for speech 
+#    """
+#    # Main acoustic input
+#    input_data = Input(name='the_input', shape=(None, input_dim))
+#    # TODO: Specify the layers in your network
+#    ...
+#    # TODO: Add softmax activation layer
+#    y_pred = ...
+#    # Specify the model
+#    model = Model(inputs=input_data, outputs=y_pred)
+#    # TODO: Specify model.output_length
+#    model.output_length = ...
+#    print(model.summary())
+#    return model
+    
+#cnn_rnn_model(input_dim, filters, kernel_size, conv_stride, conv_border_mode, units, output_dim=29):
+#deep_bd_rnn_model(input_dim, units, recur_layers=2, output_dim=29):
+
+def final_model(input_dim, filters, kernel_size, conv_stride, conv_border_mode, units, recur_layers=2, output_dim=29): #pool_size=1, 
     """ Build a deep network for speech 
     """
     # Main acoustic input
     input_data = Input(name='the_input', shape=(None, input_dim))
     # TODO: Specify the layers in your network
-    ...
-    # TODO: Add softmax activation layer
-    y_pred = ...
+
+    # Add convolutional layer with BN and Dropout
+    conv_dropout = 0.5
+    conv1 = Conv1D(filters, kernel_size, 
+                     strides=conv_stride, 
+                     padding=conv_border_mode,
+                     activation='relu',
+                     name='conv1d_1')(input_data)
+    conv1 = BatchNormalization()(conv1)
+#    conv1 = Dropout(conv_dropout)(conv1)
+#    conv1 = MaxPooling1D(pool_size=pool_size,strides=1)(conv1) #default padding is 'valid' (strides any less would make the sequence length<222)
+    
+    
+    # Add a second convolutional layer with BN and Dropout
+    
+#    conv2 = Conv1D(filters, kernel_size, 
+#                     strides=conv_stride, 
+#                     padding=conv_border_mode,
+#                     activation='relu',
+#                     name='conv1d_2')(conv1)
+#    conv2 = BatchNormalization()(conv2)
+#    conv2 = Dropout(conv_dropout)(conv2)
+#    conv2 = MaxPooling1D(pool_size=pool_size)(conv2)
+    
+    # Add multilayer RNN with dropout
+    
+    recur = conv1
+    for cellnum in range(recur_layers):
+        recur = Bidirectional( GRU( units, activation='relu', return_sequences=True, dropout = 0.0, recurrent_dropout = 0.0) )(recur) #dropout was droput_U and recurrent_dropout was dropout_W
+        recur = BatchNormalization()(recur)
+        
+#    recur = MaxPooling1D( pool_size=pool_size )( recur ) #default padding is 'valid' (strides any less would make the sequence length<222)
+    # Add a TimeDistributed(Dense(output_dim)) layer
+    time_dense = TimeDistributed(Dense(output_dim))( recur )
+    # Add softmax activation layer
+    y_pred = Activation('softmax', name='softmax')(time_dense)
+
     # Specify the model
     model = Model(inputs=input_data, outputs=y_pred)
-    # TODO: Specify model.output_length
-    model.output_length = ...
+    # Specify model.output_length
+    model.output_length = lambda x: cnn_output_length(
+        x, kernel_size, conv_border_mode, conv_stride)
+#    model.output_length = lambda x: cnn_pooling_output_length(
+#        x, kernel_size, conv_border_mode, conv_stride, pool_size=pool_size, num_cnn=1, num_pool=1)
+
+#    model.output_length = lambda x: cnn_output_length( cnn_output_length(
+#        x, kernel_size, conv_border_mode, conv_stride), kernel_size, conv_border_mode, conv_stride )
     print(model.summary())
     return model
+
+def cnn_pooling_output_length(input_length, filter_size, border_mode, stride, pool_size=1,
+                              num_cnn=1, num_pool=1, dilation=1):
+    """ Compute the length of the output sequence after 1D convolution along
+        time. Note that this function is in line with the function used in
+        Convolution1D class from Keras.
+    Params:
+        input_length (int): Length of the input sequence.
+        filter_size (int): Width of the convolution kernel.
+        border_mode (str): Only support `same` or `valid`.
+        stride (int): Stride size used in 1D convolution.
+        dilation (int)
+    """
+    if input_length is None:
+        return None
+    assert border_mode in {'same', 'valid'}
+    dilated_filter_size = filter_size + (filter_size - 1) * (dilation - 1)
+    if border_mode == 'same':
+        output_length = input_length
+    elif border_mode == 'valid':
+        output_length = input_length - dilated_filter_size + 1
+    return (output_length + stride - 1) // stride - pool_size +1
+
