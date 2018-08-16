c26025a363058f8f7c4eb0d1f3ed7e2b65edf7d0
The Gotamist <gotamist@gmail.com>
2018-08-15 18:18:46 -0700

    Edit final model for 1 conv layer and 3 layers of bidirectional GRUs.

--- 3_vui_speech_recognizer/sample_models.py
+++ 3_vui_speech_recognizer/sample_models.py
@@ -180,6 +180,8 @@ def deep_bd_rnn_model(input_dim, units, recur_layers=2, output_dim=29):
 def final_model(input_dim, filters, kernel_size, conv_stride, conv_border_mode, units, recur_layers=2, output_dim=29): #pool_size=1, 
     """ Build a deep network for speech 
     """
+#    pool_size = 2
+#    pool_strides =1 
     # Main acoustic input
     input_data = Input(name='the_input', shape=(None, input_dim))
     # TODO: Specify the layers in your network
@@ -193,19 +195,20 @@ def final_model(input_dim, filters, kernel_size, conv_stride, conv_border_mode, 
                      name='conv1d_1')(input_data)
     conv1 = BatchNormalization()(conv1)
     conv1 = Dropout(drop_fraction)(conv1)
-#    conv1 = MaxPooling1D(pool_size=pool_size,strides=1)(conv1) #default padding is 'valid' (strides any less would make the sequence length<222)
+#    conv1 = MaxPooling1D(pool_size=pool_size,strides=pool_strides)(conv1) #default padding is 'valid' (strides any less would make the sequence length<222)
     
     
     # Add a second convolutional layer with BN and Dropout
     
-    conv2 = Conv1D(filters, kernel_size, 
-                     strides=conv_stride, 
-                     padding=conv_border_mode,
-                     activation='relu',
-                     name='conv1d_2')(conv1)
-    conv2 = BatchNormalization()(conv2)
-    conv2 = Dropout(drop_fraction)(conv2)
-#    conv2 = MaxPooling1D(pool_size=pool_size)(conv2)
+#    conv2 = Conv1D(2*filters, kernel_size, 
+#                     strides=conv_stride, 
+#                     padding=conv_border_mode,
+#                     activation='relu',
+#                     name='conv1d_2')(conv1)
+#    conv2 = BatchNormalization()(conv2)
+#    conv2 = Dropout(drop_fraction)(conv2)
+    
+#    conv2 = MaxPooling1D(pool_size=pool_size, strides=pool_strides)(conv2)
     
     # Add multilayer RNN with dropout
     
@@ -214,7 +217,8 @@ def final_model(input_dim, filters, kernel_size, conv_stride, conv_border_mode, 
         recur = Bidirectional( GRU( units, activation='relu', return_sequences=True, dropout = drop_fraction, recurrent_dropout = drop_fraction) )(recur) #dropout was droput_U and recurrent_dropout was dropout_W
         recur = BatchNormalization()(recur)
         
-#    recur = MaxPooling1D( pool_size=pool_size )( recur ) #default padding is 'valid' (strides any less would make the sequence length<222)
+#    recur = MaxPooling1D( pool_size=pool_size, strides=pool_strides )( recur ) #default padding is 'valid' (strides any less would make the sequence length<222)
+
     # Add a TimeDistributed(Dense(output_dim)) layer
     time_dense = TimeDistributed(Dense(output_dim))( recur )
     # Add softmax activation layer
@@ -226,32 +230,31 @@ def final_model(input_dim, filters, kernel_size, conv_stride, conv_border_mode, 
     model.output_length = lambda x: cnn_output_length(
         x, kernel_size, conv_border_mode, conv_stride)
 #    model.output_length = lambda x: cnn_pooling_output_length(
-#        x, kernel_size, conv_border_mode, conv_stride, pool_size=pool_size, num_cnn=1, num_pool=1)
+#        x, kernel_size, conv_border_mode, conv_stride, pool_size=pool_size, num_cnn=2, num_pool=1)
 
 #    model.output_length = lambda x: cnn_output_length( cnn_output_length(
 #        x, kernel_size, conv_border_mode, conv_stride), kernel_size, conv_border_mode, conv_stride )
     print(model.summary())
     return model
 
-def cnn_pooling_output_length(input_length, filter_size, border_mode, stride, pool_size=1,
-                              num_cnn=1, num_pool=1, dilation=1):
-    """ Compute the length of the output sequence after 1D convolution along
-        time. Note that this function is in line with the function used in
-        Convolution1D class from Keras.
-    Params:
-        input_length (int): Length of the input sequence.
-        filter_size (int): Width of the convolution kernel.
-        border_mode (str): Only support `same` or `valid`.
-        stride (int): Stride size used in 1D convolution.
-        dilation (int)
-    """
-    if input_length is None:
-        return None
-    assert border_mode in {'same', 'valid'}
-    dilated_filter_size = filter_size + (filter_size - 1) * (dilation - 1)
-    if border_mode == 'same':
-        output_length = input_length
-    elif border_mode == 'valid':
-        output_length = input_length - dilated_filter_size + 1
-    return (output_length + stride - 1) // stride - pool_size +1
-
+#def cnn_pooling_output_length(input_length, filter_size, border_mode, stride, pool_size=1,
+#                              num_cnn=1, num_pool=1, dilation=1):
+#    """ Compute the length of the output sequence after 1D convolution along
+#        time. Note that this function is in line with the function used in
+#        Convolution1D class from Keras.
+#    Params:
+#        input_length (int): Length of the input sequence.
+#        filter_size (int): Width of the convolution kernel.
+#        border_mode (str): Only support `same` or `valid`.
+#        stride (int): Stride size used in 1D convolution.
+#        dilation (int)
+#    """
+#    if input_length is None:
+#        return None
+#    assert border_mode in {'same', 'valid'}
+#    dilated_filter_size = filter_size + (filter_size - 1) * (dilation - 1)
+#    if border_mode == 'same':
+#        output_length = input_length
+#    elif border_mode == 'valid':
+#        output_length = input_length - dilated_filter_size + 1
+#    return (output_length + stride - 1) // stride -pool_size + 1 
